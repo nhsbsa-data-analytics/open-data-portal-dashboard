34b06bb6caafbc90aee79a5dc1fefc1313f5928b
AdnanShroufi <adnan.shroufi@nhs.net>
2021-06-03 09:58:16 +0100

    first_prototype_ui_server
    multiple additions

--- app/ui.r
+++ app/ui.r
@@ -1,30 +1,158 @@
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
+# UI ---------------------------------------------------------------------------
+header = dashboardHeader(
+  title = "ODP - Items & NIC"
+)
 
-  # App title ----
-  titlePanel("Hello Shiny!"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-
+# Define the sidebar (on every page) -------------------------------------------
+sidebar = dashboardSidebar(
+  width = 200,
+  sidebarMenu(
+    # Static Sidebar
+    #style = "position: fixed; overflow: visible;",
+    
+    # Tab List
+    menuItem(
+      text = strong("Introduction"),
+      tabName = "introduction"
     ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-
+    menuItem(
+      text = strong("Items"),
+      tabName = "items_tab"
+    ),
+    menuItem(
+      text = strong("NIC"),
+      tabName = "nic_tab"
     )
   )
 )
+
+
+# Define the main body of the dashboard ----------------------------------------
+body = dashboardBody(
+  
+  # Main theme
+  shinyDashboardThemes(
+    theme = "grey_light"
+  ),
+  
+  # Iterate over each tab and fill
+  tabItems(
+    
+    # Tab Two ------------------------------------------------------------------
+    tabItem(
+      "items_tab",
+      fluidRow(
+        tabBox(
+          width = 12,
+          side = "left",
+          id = "items_tab_panel",
+          selected = "Region",
+          
+          # TabPanel One -------------------------------------------------------
+          tabPanel(
+            title = "Region",
+            fluidRow(
+              column(
+                width = 8,
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "Regions Items Table",
+                  reactableOutput("items_region_table", height = "300px")
+                ),
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "Regions Items Line",
+                  highchartOutput("items_region_line", height = "300px")
+                )
+              ),
+              column(
+                width = 4,
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "Regions Items Map",
+                  highchartOutput("items_region_map", height = "600px")
+                )
+              )
+            )
+          ),
+          
+          # TabPanelTwo --------------------------------------------------------
+          tabPanel(
+            title = "STP",
+            fluidRow(
+              column(
+                width = 8,
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "STP Items Table",
+                  reactableOutput("items_stp_table", height = "300px")
+                ),
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "STP Items Line",
+                  highchartOutput("items_stp_line", height = "300px")
+                )
+              ),
+              column(
+                width = 4,
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "STP Items Map",
+                  highchartOutput("items_stp_map", height = "600px")
+                )
+              )
+            )
+          ),
+          
+          # TabPanel Three -----------------------------------------------------
+          tabPanel(
+            title = "CCG",
+            fluidRow(
+              column(
+                width = 8,
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "CCG Items Table",
+                  reactableOutput("items_ccg_table", height = "300px")
+                ),
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "CCG Items Line",
+                  highchartOutput("items_ccg_line", height = "300px")
+                )
+              ),
+              column(
+                width = 4,
+                box(
+                  width = 12,
+                  solidHeader = T,
+                  title = "CCG Items Map",
+                  highchartOutput("items_ccg_map", height = "600px")
+                )
+              )
+            )
+          )
+
+          #---------------------------------------------------------------------
+        )
+      )
+    )
+  )
+)
+          
+# Define the UI ----------------------------------------------------------------
+ui = dashboardPage(
+  header, 
+  sidebar, 
+  body
+)
+
+#-------------------------------------------------------------------------------
